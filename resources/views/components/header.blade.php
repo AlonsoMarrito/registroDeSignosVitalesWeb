51a405e65112ba6645da1f3038b0a2c26e848a34
Tu Nombre <jorge.romo.23s@utzmg.edu.mx>
2025-03-08 19:05:00 -0600

    propuesta barra de navegacion

--- resources/views/components/header.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="bg-blue-500 text-white p-4">
-    <h1>{{ "Hola mundo" }}</h1> <!-- Título pasado desde la vista -->
-</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,39 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="{{ asset('css/bodyPrincipal.css') }}" rel="stylesheet" />
     </head>
     <body class="principalBody">
-        <x-header :title="'Página Principal'"/>
-        @yield('content')
+        <header class="cabeceraPrincipal">
+            <div class="seccionImage">
+                <img src="./assets/logoCompany1.jpeg" alt="Logo del producto" class="imagenBarNav">
+            </div>
+            <div class="seccionNavigationViews">
+                <button class="buttonNavegation">Inicio</button>
+                <button class="nav-item buttonNavegation">
+                    <a href="" class="dropdown-btn">HIT Solutions</a>
+                    <div class="dropdown-content">
+                        <ul>
+                            <li><a href="#">Quienes somos</a></li>
+                            <li><a href="#">Misión y visión</a></li>
+                            <li><a href="#">Historia</a></li>
+                        </ul>
+                    </div>
+                </button>
+                <button class="nav-item buttonNavegation">
+                    <a href="" class="dropdown-btn">Nuestros productos</a>
+                    <div class="dropdown-content">
+                        <ul>
+                            <li><a href="#">MDMR</a></li>
+                            <li><a href="#">Vital band</a></li>
+                        </ul>
+                    </div>
+                </button>
+                <a class="buttonNavegation" href="https://mdmr2.idgs2023s.com.mx/index.php">Usar MDMR</a>
+                <a class="buttonNavegation" href="">Usar Vital Band</a>
+            </div>
+        </header>
     </body>
-    <h1>Hola</h1>
 </html>
