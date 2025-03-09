c0357337100f9c7c778160e0a8d39deb18679283
Tu Nombre <jorge.romo.23s@utzmg.edu.mx>
2025-03-09 17:25:11 -0600

    quienes somos, mision y vision adelantadas

--- resources/views/components/hitSolutions/misionVision.blade.php
+++ resources/views/components/hitSolutions/misionVision.blade.php
@@ -2,8 +2,33 @@
 
 @section('barNav')
 <link href="{{ asset('css/inicio/textos.css') }}" rel="stylesheet" />
-<link href="{{ asset('css/inicio/subMenu.css') }}" rel="stylesheet" />
+<link href="{{ asset('css/hitSolutions/misionVison.css') }}" rel="stylesheet" />
     <div class="divPrincipal">
-        <h1>Hola mundo</h1>
+        <h1 class="titulo">HIT Solutions<br></h1>
+        <h2>¿Que es lo que buscamos?</h2>
+        <div>
+            <p class="bienvenidaAlVisonMision">
+                Desde nuestro inicio, nos hemos enfocado en un objetivo fundamental: cuidar la salud de las personas con un enfoque fresco, dinámico y comprometido. Como empresa joven, entendemos los desafíos actuales y sabemos que el acceso a soluciones de salud confiables es esencial. Por ello, trabajamos con responsabilidad y empatía, poniendo siempre a las personas en el centro de todo lo que hacemos. Buscamos ser más que un proveedor de servicios, queremos ser un apoyo real en cada etapa del camino, brindando información, herramientas y asistencia cuando más se necesita.
+            </p>
+        </div>
+        <div class="divPrincipalMisionVision">
+            <div class="misionVision-div">
+                <img src="./assets/hitSolutions/misionVision/mision.webp" alt="Logo del producto" class="imagenMisionVision_misionVision">
+                <div class="div-tetxoMisionVisio">
+                    <h3 class="tituloMisionVision">Mision</h3>
+                    <p class="tetoMisionVision">
+                        Somos una empresa joven comprometida con la salud y el bienestar de las personas. Nuestra misión es proporcionar soluciones innovadoras y accesibles que mejoren la calidad de vida, ofreciendo un servicio cercano, confiable y humano. Buscamos ser un apoyo en momentos clave, brindando atención y herramientas que permitan a cada persona cuidar de su salud de manera efectiva y segura.
+                    </p>
+                </div>
+            </div>
+            <div class="misionVision-div">
+                <div class="div-tetxoMisionVisio">
+                    <h3 class="tituloMisionVision">Mision</h3>
+                    <p class="tetoMisionVision">
+                        Aspiramos a ser un referente en el sector de la salud, reconocidos por nuestro compromiso, innovación y calidad en el servicio. Nos proyectamos como una empresa en constante crecimiento, adaptándonos a las necesidades cambiantes de la sociedad y utilizando la tecnología y el conocimiento para mejorar la vida de las personas. Queremos ser el aliado de confianza en el bienestar de nuestra comunidad, generando un impacto positivo y duradero.
+                </div>
+                <img src="./assets/hitSolutions/misionVision/vision.jpg" alt="Logo del producto" class="imagenMisionVision_misionVision">
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/components/hitSolutions/quienesSomos.blade.php
+++ resources/views/components/hitSolutions/quienesSomos.blade.php
@@ -2,8 +2,40 @@
 
 @section('barNav')
 <link href="{{ asset('css/inicio/textos.css') }}" rel="stylesheet" />
-<link href="{{ asset('css/inicio/subMenu.css') }}" rel="stylesheet" />
+<link href="{{ asset('css/hitSolutions/quienesSomos.css') }}" rel="stylesheet" />
     <div class="divPrincipal">
-        <h1>Hola mundo</h1>
+        <h1 class="titulo">HIT Solutions<br></h1>
+        <div class="divSuperior">
+            <img src="./assets/hitSolutions/quienesSomos/altoRendimiento.webp" alt="Logo del producto" class="imagenQuienesSomos_Equipo">
+            <p class="textoQuienesSomos_Equipo">
+                Somos una empresa joven y apasionada, nacida con el propósito de contribuir al bienestar de las personas. Creemos que la salud es el pilar fundamental de una vida plena, por lo que trabajamos constantemente en desarrollar soluciones innovadoras y accesibles para mejorar la calidad de vida de nuestra comunidad. Nos motiva la confianza de quienes buscan apoyo en momentos clave y nos esforzamos por ofrecer servicios y productos que marquen una diferencia real. Nuestro compromiso es claro: ser un aliado en el cuidado de la salud, brindando atención responsable, cercana y efectiva.
+            </p>
+        </div>
+        <div class="divMedio">
+            <div class="divParticipante">
+                <img src="./assets/hitSolutions/quienesSomos/alonso.jpeg" alt="Logo del producto" class="imagenQuienesSomos_Participantes">
+                <p class="textoQuienesSomos_Participante">
+                    Director corporativo <br> Programador movil
+                </p>
+            </div>
+            <div class="divParticipante">
+                <img src="./assets/hitSolutions/quienesSomos/jesus.jpeg" alt="Logo del producto" class="imagenQuienesSomos_Participantes">
+                <p class="textoQuienesSomos_Participante">
+                    Directora de diseño <br> Programador movil
+                </p>
+            </div>
+            <div class="divParticipante">
+                <img src="./assets/hitSolutions/quienesSomos/jesus.jpeg" alt="Logo del producto" class="imagenQuienesSomos_Participantes">
+                <p class="textoQuienesSomos_Participante">
+                    Director administrativo <br> Programador internet de las cosa
+                </p>
+            </div>
+        </div>
+        <div class="divSuperior">
+            <p class="textoQuienesSomos_Equipo2">
+                Somos una empresa joven con un fuerte compromiso con la salud y el bienestar de la gente. Creemos que cada persona merece acceso a soluciones que mejoren su calidad de vida, por eso trabajamos con dedicación para ofrecer alternativas innovadoras, seguras y efectivas. Nuestro enfoque es cercano y humano, porque entendemos que la salud va más allá de tratamientos y productos; es acompañamiento, confianza y compromiso. Día a día buscamos crecer y evolucionar, siempre con la meta de convertirnos en un aliado confiable en el cuidado de la salud de nuestra comunidad.
+            </p>
+            <img src="./assets/hitSolutions/quienesSomos/altoRendimiento.webp" alt="Logo del producto" class="imagenQuienesSomos_Equipo2">
+        </div>
     </div>
 @endsection
